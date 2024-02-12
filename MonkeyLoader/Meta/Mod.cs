6fb3d9f44283386350bf491c331751fa4008892d
ErrorJan <50844353+ErrorJan@users.noreply.github.com>
2024-02-12 13:58:10 +0100

    Fixes initialization of the ModComparer The ModComparer now gets the correct boolean, if it's ascending or descending.

--- MonkeyLoader/Meta/Mod.cs
+++ MonkeyLoader/Meta/Mod.cs
@@ -51,12 +51,12 @@ namespace MonkeyLoader.Meta
         /// <summary>
         /// Gets an <see cref="IComparer{T}"/> that keeps <see cref="Mod"/>s sorted in topological order.
         /// </summary>
-        public static IComparer<IMod> AscendingComparer { get; } = new ModComparer();
+        public static IComparer<IMod> AscendingComparer { get; } = new ModComparer( true );
 
         /// <summary>
         /// Gets an <see cref="IComparer{T}"/> that keeps <see cref="Mod"/>s sorted in reverse topological order.
         /// </summary>
-        public static IComparer<IMod> DescendingComparer { get; } = new ModComparer();
+        public static IComparer<IMod> DescendingComparer { get; } = new ModComparer( false );
 
         /// <inheritdoc/>
         public bool AllDependenciesLoaded
