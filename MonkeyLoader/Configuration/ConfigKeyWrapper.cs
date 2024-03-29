db980c2ba4aa7129346ee734496532a5779f447e
Arne Kiesewetter <banane9@live.de>
2024-03-29 18:59:05 +0100

    Add ConfigKey Wrappers

---
+++ MonkeyLoader/Configuration/ConfigKeyWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Configuration
+{
+    /// <summary>
+    /// Base class for config key wrappers.
+    /// </summary>
+    public abstract class ConfigKeyWrapper<TKey> : IConfigKey
+        where TKey : IConfigKey
+    {
+        /// <inheritdoc/>
+        public bool IsDefiningKey => Key.IsDefiningKey;
+
+        /// <summary>
+        /// Gets the wrapped config key.
+        /// </summary>
+        public TKey Key { get; }
+
+        /// <inheritdoc/>
+        public string Name => Key.Name;
+
+        /// <summary>
+        /// Wraps the given config key.
+        /// </summary>
+        /// <param name="key">The key to wrap.</param>
+        protected ConfigKeyWrapper(TKey key)
+        {
+            Key = key;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(IConfigKey other) => Key.Equals(other);
+    }
+
+    /// <summary>
+    /// Base class for typed config key wrappers.
+    /// </summary>
+    public abstract class TypedConfigKeyWrapper<TValue> : ConfigKeyWrapper<ITypedConfigKey<TValue>>, ITypedConfigKey<TValue>
+    {
+        /// <inheritdoc/>
+        public Type ValueType => Key.ValueType;
+
+        /// <summary>
+        /// Wraps the given typed config key.
+        /// </summary>
+        /// <param name="typedKey">The typed key to wrap.</param>
+        protected TypedConfigKeyWrapper(ITypedConfigKey<TValue> typedKey) : base(typedKey)
+        { }
+    }
+}
---
+++ MonkeyLoader/Configuration/DefiningConfigKeyWrapper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.Configuration
+{
+    /// <summary>
+    /// Base class for defining config key wrappers.
+    /// </summary>
+    public abstract class DefiningConfigKeyWrapper<TValue> : IDefiningConfigKey<TValue>
+    {
+        /// <inheritdoc/>
+        public Config Config => Key.Config;
+
+        /// <inheritdoc/>
+        public string? Description => Key.Description;
+
+        /// <inheritdoc/>
+        public bool HasChanges
+        {
+            get => Key.HasChanges;
+            set => Key.HasChanges = value;
+        }
+
+        /// <inheritdoc/>
+        public bool HasDescription => Key.HasDescription;
+
+        /// <inheritdoc/>
+        public bool HasValue => Key.HasValue;
+
+        /// <inheritdoc/>
+        public bool InternalAccessOnly => Key.InternalAccessOnly;
+
+        /// <inheritdoc/>
+        public bool IsDefiningKey => Key.IsDefiningKey;
+
+        /// <summary>
+        /// Gets the wrapped defining config key.
+        /// </summary>
+        public IDefiningConfigKey<TValue> Key { get; }
+
+        /// <inheritdoc/>
+        public string Name => Key.Name;
+
+        /// <inheritdoc/>
+        public ConfigSection Section
+        {
+            get => Key.Section;
+            set => Key.Section = value;
+        }
+
+        /// <inheritdoc/>
+        public Type ValueType => Key.ValueType;
+
+        /// <summary>
+        /// Wraps the given defining config key.
+        /// </summary>
+        /// <param name="definingKey">The defining key to wrap.</param>
+        protected DefiningConfigKeyWrapper(IDefiningConfigKey<TValue> definingKey)
+        {
+            Key = definingKey;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(IConfigKey other) => Key.Equals(other);
+
+        /// <inheritdoc/>
+        public TValue? GetValue() => Key.GetValue();
+
+        object? IDefiningConfigKey.GetValue() => ((IDefiningConfigKey)Key).GetValue();
+
+        /// <inheritdoc/>
+        public void SetValue(TValue value, string? eventLabel = null) => Key.SetValue(value, eventLabel);
+
+        /// <inheritdoc/>
+        public void SetValue(object? value, string? eventLabel) => Key.SetValue(value, eventLabel);
+
+        /// <inheritdoc/>
+        public bool TryComputeDefault(out TValue? defaultValue) => Key.TryComputeDefault(out defaultValue);
+
+        /// <inheritdoc/>
+        public bool TryComputeDefault(out object? defaultValue) => Key.TryComputeDefault(out defaultValue);
+
+        /// <inheritdoc/>
+        public bool TryGetValue(out TValue? value) => Key.TryGetValue(out value);
+
+        /// <inheritdoc/>
+        public bool TryGetValue(out object? value) => Key.TryGetValue(out value);
+
+        /// <inheritdoc/>
+        public bool TrySetValue(TValue value, string? eventLabel = null) => Key.TrySetValue(value, eventLabel);
+
+        /// <inheritdoc/>
+        public bool TrySetValue(object? value, string? eventLabel) => Key.TrySetValue(value, eventLabel);
+
+        /// <inheritdoc/>
+        public bool Unset() => Key.Unset();
+
+        /// <inheritdoc/>
+        public bool Validate(TValue value) => Key.Validate(value);
+
+        /// <inheritdoc/>
+        public bool Validate(object? value) => Key.Validate(value);
+
+        /// <inheritdoc/>
+        public event ConfigKeyChangedEventHandler<TValue>? Changed
+        {
+            add => Key.Changed += value;
+            remove => Key.Changed -= value;
+        }
+
+        event ConfigKeyChangedEventHandler? IDefiningConfigKey.Changed
+        {
+            add => ((IDefiningConfigKey)Key).Changed += value;
+            remove => ((IDefiningConfigKey)Key).Changed -= value;
+        }
+    }
+}
