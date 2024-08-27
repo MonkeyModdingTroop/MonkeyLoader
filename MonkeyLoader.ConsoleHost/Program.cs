9ae1564171f3642fae965612349cfd7a41467c3b
Arne Kiesewetter <banane9@live.de>
2024-08-27 20:42:42 +0200

    Add ConsoleHost mini-app

---
+++ MonkeyLoader.ConsoleHost/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Pipes;
+
+namespace MonkeyLoader.ConsoleHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var appName = args.FirstOrDefault() ?? "MonkeyLoader";
+
+            using var pipeServer = new NamedPipeServerStream($"MonkeyLoader.ConsoleHost.{appName}", PipeDirection.In);
+            pipeServer.WaitForConnection();
+
+            var reader = new StreamReader(pipeServer);
+
+            try
+            {
+                var i = 0;
+                while (i >= 0)
+                {
+                    i = reader.Read();
+
+                    if (i >= 0)
+                        Console.Write(Convert.ToChar(i));
+                }
+            }
+            catch (IOException)
+            {
+                //error handling code here
+            }
+        }
+    }
+}
