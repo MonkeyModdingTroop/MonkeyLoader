7e8ef479a24056e100d6760cb2fff1b2fca5eb4a
Arne Kiesewetter <banane9@live.de>
2024-10-07 21:52:29 +0200

    Add (Try)FindNearestParent extension methods for identifiables

--- MonkeyLoader/IdentifiableExtensions.cs
+++ MonkeyLoader/IdentifiableExtensions.cs
@@ -1,14 +1,52 @@
 ﻿using MonkeyLoader.Meta;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonkeyLoader
 {
+    /// <summary>
+    /// Contains extension methods dealing with <see cref="IIdentifiable"/>s.
+    /// </summary>
     public static class IdentifiableExtensions
     {
+        /// <summary>
+        /// Finds the nearest <typeparamref name="TParentIdentifiable"/> parent of this <paramref name="identifiable"/>
+        /// that satisfies the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="TParentIdentifiable">The type of the parent <see cref="IIdentifiable"/> to look for.</typeparam>
+        /// <param name="identifiable">The <see cref="IIdentifiable"/> to start looking from.</param>
+        /// <param name="predicate">The predicate that a potential parent must satisfy.</param>
+        /// <returns>The parent satisfying the <paramref name="predicate"/>.</returns>
+        /// <exception cref="InvalidOperationException">When no suitable parent was found.</exception>
+        public static TParentIdentifiable FindNearestParent<TParentIdentifiable>(this IIdentifiable identifiable, Predicate<TParentIdentifiable> predicate)
+            where TParentIdentifiable : IIdentifiable
+        {
+            if (!identifiable.TryFindNearestParent(predicate, out var parentIdentifiable))
+                throw new InvalidOperationException("No suitable parent found!");
+
+            return parentIdentifiable;
+        }
+
+        /// <summary>
+        /// Finds the nearest <typeparamref name="TParentIdentifiable"/> parent of this <paramref name="identifiable"/>.
+        /// </summary>
+        /// <typeparam name="TParentIdentifiable">The type of the parent <see cref="IIdentifiable"/> to look for.</typeparam>
+        /// <param name="identifiable">The <see cref="IIdentifiable"/> to start looking from.</param>
+        /// <returns>The found parent.</returns>
+        /// <exception cref="InvalidOperationException">When no suitable parent was found.</exception>
+        public static TParentIdentifiable FindNearestParent<TParentIdentifiable>(this IIdentifiable identifiable)
+            where TParentIdentifiable : IIdentifiable
+        {
+            if (!identifiable.TryFindNearestParent(out TParentIdentifiable? parentIdentifiable))
+                throw new InvalidOperationException("No suitable parent found!");
+
+            return parentIdentifiable;
+        }
+
         public static IIdentifiableSearch<TIdentifiable> Get<TIdentifiable>(this IIdentifiableCollection<TIdentifiable> identifiableCollection)
             where TIdentifiable : IIdentifiable
             => new IdentifiableSearch<TIdentifiable>(identifiableCollection.Items);
@@ -33,6 +71,57 @@ namespace MonkeyLoader
         public static IEnumerable<TIdentifiable> GetAll<TIdentifiable>(this INestedIdentifiableCollection<TIdentifiable> nestedIdentifiableCollection)
             where TIdentifiable : INestedIdentifiable
             => nestedIdentifiableCollection.Items;
+
+        /// <summary>
+        /// Tries to find the nearest <typeparamref name="TParentIdentifiable"/> parent of this <paramref name="identifiable"/>
+        /// that satisfies the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="TParentIdentifiable">The type of the parent <see cref="IIdentifiable"/> to look for.</typeparam>
+        /// <param name="identifiable">The <see cref="IIdentifiable"/> to start looking from.</param>
+        /// <param name="predicate">The predicate that a potential parent must satisfy.</param>
+        /// <param name="parentIdentifiable">The parent satisfying the <paramref name="predicate"/> if found; otherwise, <c>default</c>.</param>
+        /// <returns><c>true</c> if a parent satisfying the <paramref name="predicate"/> was found; otherwise, <c>false</c>.</returns>
+        public static bool TryFindNearestParent<TParentIdentifiable>(this IIdentifiable identifiable,
+                Predicate<TParentIdentifiable> predicate, [NotNullWhen(true)] out TParentIdentifiable? parentIdentifiable)
+            where TParentIdentifiable : IIdentifiable
+        {
+            while (identifiable.TryFindNearestParent(out parentIdentifiable))
+            {
+                if (predicate(parentIdentifiable))
+                    return true;
+
+                if (parentIdentifiable is not INestedIdentifiable)
+                    break;
+
+                identifiable = (INestedIdentifiable)parentIdentifiable;
+            }
+
+            parentIdentifiable = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Tries to find the nearest <typeparamref name="TParentIdentifiable"/> parent of this <paramref name="identifiable"/>.
+        /// </summary>
+        /// <typeparam name="TParentIdentifiable">The type of the parent <see cref="IIdentifiable"/> to look for.</typeparam>
+        /// <param name="identifiable">The <see cref="IIdentifiable"/> to start looking from.</param>
+        /// <param name="parentIdentifiable">The parent if found; otherwise, <c>default</c>.</param>
+        /// <returns><c>true</c> if a parent was found; otherwise, <c>false</c>.</returns>
+        public static bool TryFindNearestParent<TParentIdentifiable>(this IIdentifiable identifiable, [NotNullWhen(true)] out TParentIdentifiable? parentIdentifiable)
+            where TParentIdentifiable : IIdentifiable
+        {
+            while (identifiable is not TParentIdentifiable and INestedIdentifiable nestedCurrent)
+                identifiable = nestedCurrent.Parent;
+
+            if (identifiable is TParentIdentifiable parent)
+            {
+                parentIdentifiable = parent;
+                return true;
+            }
+
+            parentIdentifiable = default;
+            return false;
+        }
 
         public static INestedIdentifiableOwnerTrySearch<TIdentifiable> TryGet<TIdentifiable>(this INestedIdentifiableOwner<TIdentifiable> nestedIdentifiableOwner)
             where TIdentifiable : INestedIdentifiable
