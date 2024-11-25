eb70ca5ee152e13564ed1ffd1f319f6a34ea2f8a
Arne Kiesewetter <banane9@live.de>
2024-11-25 21:41:46 +0100

    Add bidirectional config key binding

---
+++ MonkeyLoader/Configuration/ConfigSystemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MonkeyLoader.Meta;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyLoader.Configuration
+{
+    /// <summary>
+    /// Contains extensions methods related to the config system.
+    /// </summary>
+    public static class ConfigSystemExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="IConfigKeyChangedEventArgs.Label">Label</see> for
+        /// a propagated <see cref="IDefiningConfigKey.Changed">Changed</see> event.<br/>
+        /// The created event label will have this format:
+        /// <c>$"{<paramref name="baseLabel"/>}:{<paramref name="changedEventArgs"/>.<see cref="IConfigKeyChangedEventArgs.Key">Key</see>.<see cref="IIdentifiable.FullId">FullId</see>}:{<paramref name="changedEventArgs"/>.<see cref="IConfigKeyChangedEventArgs.Label">Label</see>}"</c>.
+        /// </summary>
+        /// <param name="changedEventArgs">The changed event that triggered this propagation.</param>
+        /// <param name="baseLabel">The new base label for the event.</param>
+        /// <returns>The formatted label for the propagated event.</returns>
+        public static string GetPropagatedEventLabel(this IConfigKeyChangedEventArgs changedEventArgs, string baseLabel)
+            => $"{baseLabel}:{changedEventArgs.Key.FullId}:{changedEventArgs.Label}";
+    }
+}
