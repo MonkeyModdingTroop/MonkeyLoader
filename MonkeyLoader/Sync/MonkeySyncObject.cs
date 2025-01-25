ab786fefadb974600a72e860a495e73ac2fed5a7
Arne Kiesewetter <banane9@live.de>
2025-01-25 16:00:48 +0100

    Add initial objects for MonkeySync system

---
+++ MonkeyLoader/Sync/MonkeySyncObject.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MonkeyLoader.Sync
+{
+    /// <summary>
+    /// Defines the non-generic interface for <see cref="MonkeySyncObject{TLink}">MonkeySync objects</see>.
+    /// </summary>
+    public interface IMonkeySyncObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets whether this sync object has a <see cref="LinkObject">link object</see>.
+        /// </summary>
+        [MemberNotNullWhen(true, nameof(LinkObject))]
+        public bool HasLinkObject { get; }
+
+        /// <summary>
+        /// Gets whether this sync object has a valid link.
+        /// </summary>
+        public bool IsLinkValid { get; }
+
+        /// <summary>
+        /// Gets the link object used by this sync object.
+        /// </summary>
+        [MaybeNull]
+        public object LinkObject { get; }
+    }
+
+    /// <summary>
+    /// Defines the generic interface for <see cref="MonkeySyncObject{TLink}">MonkeySync objects</see>.
+    /// </summary>
+    public interface IMonkeySyncObject<out TLink> : IMonkeySyncObject
+        where TLink : class
+    {
+        /// <inheritdoc cref="IMonkeySyncObject.LinkObject"/>
+        [MaybeNull]
+        public new TLink LinkObject { get; }
+    }
+
+    public abstract class MonkeySyncObject<TLink> : IMonkeySyncObject<TLink>
+        where TLink : class
+    {
+        /// <inheritdoc/>
+        [MemberNotNullWhen(true, nameof(LinkObject))]
+        public bool HasLinkObject => LinkObject is not null;
+
+        /// <inheritdoc/>
+        public abstract bool IsLinkValid { get; }
+
+        /// <inheritdoc/>
+        public TLink LinkObject { get; }
+
+        object IMonkeySyncObject.LinkObject => LinkObject;
+
+        /// <summary>
+        /// Creates a new instance of this sync object with the given link object.
+        /// </summary>
+        /// <param name="linkObject">The link object used by this sync object.</param>
+        protected MonkeySyncObject(TLink linkObject)
+        {
+            LinkObject = linkObject;
+        }
+
+        /// <summary>
+        /// Triggers the <see cref="PropertyChanged">PropertyChanged</see>
+        /// event with the given <paramref name="propertyName"/>.
+        /// </summary>
+        /// <remarks>
+        /// This is automatically called for any <see cref="MonkeySyncValue{T}"/> properties.
+        /// </remarks>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var eventData = new PropertyChangedEventArgs(propertyName);
+
+            PropertyChanged?.Invoke(this, eventData);
+        }
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ MonkeyLoader/Sync/MonkeySyncValue.cs
@@ -0,0 +1,82 @@
+﻿using MonkeyLoader.Meta;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyLoader.Sync
+{
+    /// <summary>
+    /// Defines the non-generic interface for <see cref="IMonkeySyncValue"/>s.
+    /// </summary>
+    public interface IMonkeySyncValue : INotifyValueChanged
+    {
+        /// <summary>
+        /// Gets or sets the internal value of this sync value.
+        /// </summary>
+        public object? Value { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the generic interface for <see cref="IMonkeySyncValue"/>s.
+    /// </summary>
+    /// <typeparam name="T">The type of the <see cref="Value">Value</see>.</typeparam>
+    public interface IMonkeySyncValue<T> : IReadOnlyMonkeySyncValue<T>, IWriteOnlyMonkeySyncValue<T>
+    {
+        /// <inheritdoc cref="IMonkeySyncValue.Value"/>
+        public new T Value { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the interface for readonly <see cref="IMonkeySyncValue"/>s.
+    /// </summary>
+    /// <remarks>
+    /// This interface exists purely to facilitate keeping a covariant list of sync values.
+    /// </remarks>
+    /// <typeparam name="T">The type of the <see cref="Value">Value</see>.</typeparam>
+    public interface IReadOnlyMonkeySyncValue<out T> : IMonkeySyncValue
+    {
+        /// <summary>
+        /// Gets the internal value of this sync value.
+        /// </summary>
+        public new T Value { get; }
+    }
+
+    /// <summary>
+    /// Defines the interface for writeonly <see cref="IMonkeySyncValue"/>s.
+    /// </summary>
+    /// <remarks>
+    /// This interface exists purely to facilitate keeping a contravariant list of sync values.
+    /// </remarks>
+    /// <typeparam name="T">The type of the <see cref="Value">Value</see>.</typeparam>
+    public interface IWriteOnlyMonkeySyncValue<in T> : IMonkeySyncValue
+    {
+        /// <summary>
+        /// Sets the internal value of this sync value.
+        /// </summary>
+        public new T Value { set; }
+    }
+
+    public class MonkeySyncValue<T> : IMonkeySyncValue<T>
+    {
+        private T _value;
+
+        /// <inheritdoc/>
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (ReferenceEquals(_value, value))
+                    return;
+
+                _value = value;
+            }
+        }
+
+        object? IMonkeySyncValue.Value
+        {
+            get => Value;
+            set => Value = (T)value!;
+        }
+    }
+}
