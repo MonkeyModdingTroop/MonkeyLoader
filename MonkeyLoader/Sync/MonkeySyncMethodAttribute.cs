5a53d068d04cc3ad744a195ee47d7e43063faea3
Arne Kiesewetter <banane9@live.de>
2025-01-25 16:00:48 +0100

    Add initial objects for MonkeySync system

---
+++ MonkeyLoader/Sync/MonkeySyncMethodAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonkeyLoader.Sync
+{
+    /// <summary>
+    /// Marks a <see cref="MonkeySyncObject{T}"/>'s instance method as
+    /// being triggerable through the Sync system.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class MonkeySyncMethodAttribute : MonkeyLoaderAttribute
+    { }
+}
