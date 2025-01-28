d4e0553230cf6ea36a5fd0262d4e4e849e4b9588
Arne Kiesewetter <banane9@live.de>
2025-01-28 23:05:33 +0100

    Add MonkeySyncObjectRegistry

--- MonkeyLoader/Sync/MonkeySyncMethodAttribute.cs
+++ MonkeyLoader/Sync/MonkeySyncMethodAttribute.cs
@@ -3,8 +3,8 @@
 namespace MonkeyLoader.Sync
 {
     /// <summary>
-    /// Marks a <see cref="MonkeySyncObject{T}"/>'s instance method as
-    /// being triggerable through the Sync system.
+    /// Marks a <see cref="MonkeySyncObject{TSyncObject, TSyncValue, TLink}">MonkeySync
+    /// object's</see> instance method as being triggerable through the MonkeySync system.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public sealed class MonkeySyncMethodAttribute : MonkeyLoaderAttribute
