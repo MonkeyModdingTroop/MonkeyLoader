2631369a87d7030197ca119628d38230fe910c03
Arne Kiesewetter <banane9@live.de>
2025-05-11 14:32:25 +0200

    Add option to provide default calculation for monkey's EnabledToggles. Fix #59

--- MonkeyLoader/Meta/MonkeyTogglesConfigSection.cs
+++ MonkeyLoader/Meta/MonkeyTogglesConfigSection.cs
@@ -44,23 +44,35 @@ namespace MonkeyLoader.Meta
         public static ITypedConfigKey<bool> GetTemplateKey(IMonkey monkey)
             => new ConfigKey<bool>(monkey.Id);
 
+        /// <remarks>
+        /// The default for toggles created with this method is <c>true</c>.
+        /// </remarks>
+        /// <inheritdoc cref="GetToggle(IMonkey, Func{bool})"/>
+        public IDefiningConfigKey<bool> GetToggle(IMonkey monkey)
+            => GetToggle(monkey, static () => true);
+
         /// <summary>
-        /// Gets or creates the toggle config item for the given (early) monkey.
+        /// Gets or creates the toggle config item for the given (early) monkey,
+        /// while passing along the given method to compute its default state.
         /// </summary>
         /// <param name="monkey">
         /// The (early) monkey to get the key for. Must belong to the same
         /// <see cref="Meta.Mod"/> and support <see cref="IMonkey.CanBeDisabled">being disabled</see>.
         /// </param>
+        /// <param name="computeDefault">The function that computes the default state for this toggle.</param>
         /// <returns>The toggle config item for the given (early) monkey.</returns>
         /// <exception cref="ArgumentNullException">When the <paramref name="monkey"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentException">
         /// When the <paramref name="monkey"/> is from another <see cref="Meta.Mod"/>
         /// or doesn't support <see cref="IMonkey.CanBeDisabled">being disabled</see>.
         /// </exception>
-        public IDefiningConfigKey<bool> GetToggle(IMonkey monkey)
+        public IDefiningConfigKey<bool> GetToggle(IMonkey monkey, Func<bool> computeDefault)
         {
-            if (monkey == null)
+            if (monkey is null)
                 throw new ArgumentNullException(nameof(monkey));
+
+            if (computeDefault is null)
+                throw new ArgumentNullException(nameof(computeDefault));
 
             if (monkey.Mod != Mod || !monkey.CanBeDisabled)
                 throw new ArgumentException("Monkey doesn't belong to this section's mod or can't be disabled!");
@@ -68,7 +80,7 @@ namespace MonkeyLoader.Meta
             if (!_togglesByMonkey.TryGetValue(monkey, out var toggleKey))
             {
                 toggleKey = GetOrCreateDefiningKey(GetTemplateKey(monkey),
-                    $"Whether the {(monkey is IEarlyMonkey ? "Early Monkey" : "Monkey")} {monkey.Name} should be active.", () => true, true);
+                    $"Whether the {(monkey is IEarlyMonkey ? "Early Monkey" : "Monkey")} {monkey.Name} should be active.", computeDefault, true);
 
                 _togglesByMonkey.Add(monkey, toggleKey);
             }
