56cf1d7aae6a459e0146a946ae3c09ccc738bc64
Arne Kiesewetter <banane9@live.de>
2025-12-04 23:25:10 +0100

    Add AssemblyLoadLoadStrategy for pre-net5. Tweak implementations

--- MonkeyLoader/AssemblyLoadLoadStrategy.cs
+++ MonkeyLoader/AssemblyLoadLoadStrategy.cs
@@ -1,40 +1,32 @@
-﻿#if NET5_0_OR_GREATER
+﻿#if !NET5_0_OR_GREATER
 
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Loader;
 
 namespace MonkeyLoader
 {
     /// <summary>
     /// Implements an <see cref="IAssemblyLoadStrategy"/> that uses
-    /// this assembly's <see cref="AssemblyLoadContext"/>.
+    /// <see cref="Assembly.Load(string)"/> and <see cref="Assembly.Load(byte[], byte[])"/>.
     /// </summary>
-    internal sealed class AssemblyLoadContextLoadStrategy : IAssemblyLoadStrategy
+    internal sealed class AssemblyLoadLoadStrategy : IAssemblyLoadStrategy
     {
-        private readonly AssemblyLoadContext _assemblyLoadContext;
-
-        public AssemblyLoadContextLoadStrategy()
-        {
-            _assemblyLoadContext = AssemblyLoadContext.GetLoadContext(typeof(AssemblyLoadContextLoadStrategy).Assembly)!;
-        }
-
         /// <inheritdoc/>
         public Assembly LoadFile(string assemblyPath)
         {
-            if (string.IsNullOrWhiteSpace(assemblyPath))
+            if (string.IsNullOrEmpty(assemblyPath))
                 throw new ArgumentException("Assembly path cannot be null, empty, or whitespace.", nameof(assemblyPath));
 
-            Debug.WriteLine($"AssemblyLoadContextLoadStrategy: Loading assembly from path: {assemblyPath}");
+            Debug.WriteLine($"AssemblyLoadLoadStrategy: Loading assembly from path: {assemblyPath}");
 
             // Hack: Check if we already have a DLL with this name loaded and return it if so
-            // This is not correct, but it works for now, we need to have the game in a separate load context in the future
+            // This is not correct, but it works for now, we need to have the game in a separate load  in the future
             // Concretely, both ML and Reso use Mono.Cecil at different versions
             var assemblyName = Path.GetFileName(assemblyPath);
-            var loadedAssembly = _assemblyLoadContext.Assemblies
+            var loadedAssembly = AppDomain.CurrentDomain.GetAssemblies()
                 .FirstOrDefault(a => !string.IsNullOrWhiteSpace(a.Location) && Path.GetFileName(a.Location).Equals(assemblyName, StringComparison.OrdinalIgnoreCase));
 
             if (loadedAssembly is not null)
@@ -43,19 +35,18 @@ namespace MonkeyLoader
                 return loadedAssembly;
             }
 
-            return _assemblyLoadContext.LoadFromAssemblyPath(assemblyPath);
+            return Assembly.Load(assemblyPath);
         }
 
         /// <inheritdoc/>
         public Assembly Load(byte[] assemblyBytes, byte[]? pdbBytes = null)
         {
-            Debug.WriteLine("AssemblyLoadContextLoadStrategy: Loading assembly from byte array.");
+            Debug.WriteLine("AssemblyLoadLoadStrategy: Loading assembly from byte array.");
 
             if (assemblyBytes?.Length is not > 0)
                 throw new ArgumentException("Assembly bytes cannot be null or empty.", nameof(assemblyBytes));
 
-            return _assemblyLoadContext.LoadFromStream(new MemoryStream(assemblyBytes),
-                pdbBytes?.Length is > 0 ? new MemoryStream(pdbBytes) : null);
+            return Assembly.Load(assemblyBytes, pdbBytes);
         }
     }
 }
